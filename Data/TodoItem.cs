728ecd70e7d19b3a7551e8540d20703e2cb3cd9f
Jason Zhang <jason789.zhang@gmail.com>
2025-02-14 22:39:50 -0700

    push final work

---
+++ Data/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Data;
+
+public class TodoItem
+{
+    public string? Title { get; set; }
+    public bool IsDone { get; set; } = false;
+}
